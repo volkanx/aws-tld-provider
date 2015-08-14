655c1af80f601b485b12ed2c5c874f9f2398896e
volkanp <vpaksoy@rcpsych.ac.uk>
2015-08-14 15:32:02 +0100

    Updated gitignore

--- TldProvider.Core/Properties/AssemblyInfo.cs
+++ TldProvider.Core/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TldProvider.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library to parse AWS documentation and return the list of supported TLDs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Volkan Paksoy")]
 [assembly: AssemblyProduct("TldProvider.Core")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
